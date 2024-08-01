56bb833dd13a1cd1180b8b81cdd4ea65f70490ff
Dhruv-Langaliya <23dcs053@charusat.edu.in>
2024-08-01 14:46:23 +0530

    Create part_3_12th.java

---
+++ Part - 3/part_3_12th.java
@@ -0,0 +1,11 @@
+public class part_3_12th
+{
+	public static void main(String args[])
+	{
+		System.out.println("In rupees:" +args[0]);
+		int a =Integer.parseInt(args[0]);
+		
+		int b=a*100;
+		System.out.println("In pound:"+ b);
+}
+}
